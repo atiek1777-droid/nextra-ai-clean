5e5cafa2df012e385aad5173eb50703e6c9f7bee
atqqk <atiek1777@gmail.com>
2026-09-14 02:49:19 +0300

    fix: broken Object.assign

--- next.config.js
+++ next.config.js
@@ -3,4 +3,4 @@ const nextConfig = {
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true }
 };
-module.exports = Object.assign(, { async headers(){ return [ { source: "/:path*.html", headers: [{ key: "Cache-Control", value: "no-cache, no-store, must-revalidate, max-age=0" }] }, { source: "/:path((?!_next/static|_next/image|favicon.ico|icons|assets).*)", headers: [{ key: "Cache-Control", value: "no-cache, no-store, must-revalidate, max-age=0" }] }, { source: "/_next/static/:path*", headers: [{ key: "Cache-Control", value: "public, max-age=31536000, immutable" }] } ]; } });
+module.exports = Object.assign(nextConfig, { async headers(){ return [ { source: "/:path*.html", headers: [{ key: "Cache-Control", value: "no-cache, no-store, must-revalidate, max-age=0" }] }, { source: "/:path((?!_next/static|_next/image|favicon.ico|icons|assets).*)", headers: [{ key: "Cache-Control", value: "no-cache, no-store, must-revalidate, max-age=0" }] }, { source: "/_next/static/:path*", headers: [{ key: "Cache-Control", value: "public, max-age=31536000, immutable" }] } ]; } });
