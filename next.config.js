662cdb267831ddeaf18fa5723eb9702556a117ec
atqqk <atiek1777@gmail.com>
2026-09-14 02:27:28 +0300

    perf: cache-control headers

--- next.config.js
+++ next.config.js
@@ -3,4 +3,4 @@ const nextConfig = {
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true }
 };
-module.exports = nextConfig;
+module.exports = Object.assign(, { async headers(){ return [ { source: "/:path*.html", headers: [{ key: "Cache-Control", value: "no-cache, no-store, must-revalidate, max-age=0" }] }, { source: "/:path((?!_next/static|_next/image|favicon.ico|icons|assets).*)", headers: [{ key: "Cache-Control", value: "no-cache, no-store, must-revalidate, max-age=0" }] }, { source: "/_next/static/:path*", headers: [{ key: "Cache-Control", value: "public, max-age=31536000, immutable" }] } ]; } });
